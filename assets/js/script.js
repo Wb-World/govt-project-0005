bd86d70f028f7e197d89e913ecaae384b3104461
Mohamed Hathim <159799375+MH786-coder@users.noreply.github.com>
2025-09-29 22:54:12 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+/*
 const data = [
     { ward: 1, habitation: "KUNNUKADU", streets: ["NEW MAJITH STREET", "MUTHUMARIYAMMAN KOVIL STREET", "CARMEL NAGAR"] },
     { ward: 2, habitation: "KOVALAM KUPPAM", streets: ["BAJANAI KOVIL STREET"] },
@@ -10,7 +10,59 @@ const data = [
     { ward: 8, habitation: "SEMMENCHERRY", streets: ["PADAVETTAMMAN KOVIL STREET"] },
     { ward: 9, habitation: "SEMMENCHERRY KUPPAM", streets: ["NAGALAMMAN KOVIL STREET"] }
 ];
-
+*/
+const data = [
+    { 
+        ward: 1, 
+        habitation: "Kovalam", 
+        streets: ["Kailasanathar Kovil Street", "Beach Road"] 
+    },
+    { 
+        ward: 2, 
+        habitation: "New Majith Street", 
+        streets: ["New Majith Street"] 
+    },
+    { 
+        ward: 3, 
+        habitation: "Kottai Colony", 
+        streets: ["Madha Kovil Street"] 
+    },
+    { 
+        ward: 4, 
+        habitation: "Middle Colony", 
+        streets: ["Middle Colony"] 
+    },
+    { 
+        ward: 5, 
+        habitation: "Nachiyar Kulam", 
+        streets: ["Nachiyar Kulam", "Desai Chetty Street", "Vanigar Street", "Sengeniyanman Kovil Street"] 
+    },
+    { 
+        ward: 6, 
+        habitation: "Kundrukadu", 
+        streets: ["Kundrukadu", "Carmel Nagar", "Teachers Colony"] 
+    },
+    { 
+        ward: 7, 
+        habitation: "Ansari Nagar", 
+        streets: ["Ansari Nagar", "Dharga Street", "Padavattanman Kovil Street", "Egavaillyanman Kovil Street", "Pillaiyar Kovil Street"] 
+    },
+    { 
+        ward: 8, 
+        habitation: "Semmencheri", 
+        streets: ["Semmencheri Kuppam", "Bajanai Kovil Street", "Kamiyanman Kovil Street"] 
+    },
+    { 
+        ward: 9, 
+        habitation: "Kovalam Kuppam", 
+        streets: ["Kovalam Kuppam"] 
+    },
+    { 
+        ward: 10, 
+        habitation: "Fisheries Colony", 
+        streets: ["Fisheries Colony"] 
+    }
+];
 const tableBody = document.getElementById("table-body");
 data.forEach(({ ward, habitation, streets }, index) => {
     streets.forEach((street, i) => {
